0492f6665fedbca2d44f13b5a0a68a4bb98951b8
raiyanawinata <raiyanawinata75@gmail.com>
2023-06-29 22:09:46 +0700

    HUH HAH

--- src/route/BasePages.js
+++ src/route/BasePages.js
@@ -5,28 +5,20 @@ import Dashboard from '../modules/dash/Dashboard'
 import TabelNav from '../modules/pinjam/tabel/TabelNav'
 import Pinjam from '../modules/pinjam/Pinjam'
 import FormPinjam from '../modules/pinjam/tabel/FormPinjam'
+import Koleksi from '../modules/koleksi/Koleksi'
+import Login from '../modules/login/Login'
 
 
 export default function BasePages() {
   return (
     <Routes>
-            <Route index element={< Dashboard/>} />
-            <Route path='/dasboard' element={<Dashboard />} />
-            <Route path='/koleksi-buku' element={<Dashboard/>} />
+            <Route index element={<Pinjam/>} />
+            <Route path='/koleksi-buku' element={<Koleksi/>} />
             <Route path='/data-peminjaman' element={<Pinjam/>} />
             <Route path='/formPinjam' element={<FormPinjam/>}/>
+            {/* <Route path='/logout' element={<Login/>} /> */}
             
-                
-                {/* <Route path="reels" element={<Reels />} />
-                <Route path="fyp" element={<FYP />} /> */}
-            {/* </Route> */}
-            {/* <Route path='master-data' element={<MasterData />} >
-                <Route path="products" element={<Products />} />
-                <Route path="users" element={<Users />} />
-            </Route>
-            <Route path='messages' element={<Messages />} />
-            <Route path='profile' element={<Profiles />} />
-            <Route path='*' element={<Error404 />} /> */}
+            
         </Routes>
   )
 }
