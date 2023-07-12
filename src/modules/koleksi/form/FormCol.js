b5148f8d4cb2e3f1274b28672ac768079d1a5f4c
raiyanawinata <raiyanawinata75@gmail.com>
2023-07-12 17:15:59 +0700

    FE

--- src/modules/koleksi/form/FormCol.js
+++ src/modules/koleksi/form/FormCol.js
@@ -1,3 +1,227 @@
+// import React, { Component } from "react";
+// import { Form, Button, Row, Col, Modal } from "react-bootstrap";
+// import "./formC.css";
+// import axios from "axios";
+
+// class FormCol extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       idBuku: "",
+//       nmBuku: "",
+//       penulis: "",
+//       thnTerbit: "",
+//       tglMasuk: "",
+//       jumlahBuku: "",
+//       noRak: "",
+//       showModal:false,
+//     };
+//   }
+
+
+//   handleSubmit = async (event) => {
+//     event.preventDefault();
+//     const { idBuku,nmBuku,penulis,thnTerbit,tglMasuk,jumlahBuku,noRak } = this.state;
+
+//     try {
+//       // Kirim permintaan POST ke backend menggunakan Axios
+//       const response = await axios.post("http://localhost:4000/create/collection", {
+//         idBuku,
+//         nmBuku,
+//         penulis,
+//         thnTerbit,
+//         tglMasuk,
+//         jumlahBuku,
+//         noRak,
+//         showModal:false,
+//       });
+
+//       console.log(response.data); // Response dari backend (data peminjaman baru)
+
+//       // Tambahkan data baru ke dalam tabel menggunakan fungsi addNewData
+//     this.props.addNewData(response.data);
+
+//       //menampilkan modal
+//       this.setState({showModal:true});
+
+//       // Reset form setelah submit
+//       this.setState({
+//        idBuku:"",
+//        nmBuku:"",
+//        penulis: "",
+//        thnTerbit:"",
+//        tglMasuk:"",
+//        jumlahBuku:"",
+//        noRak:"",
+//       });
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//     handleModalClose = ()=>{
+//       //menyembunyikan modal
+//       this.setState({showModal:false});
+//     }
+
+//     //memformat tanggal sesuai dengan format yang diinginkan.
+//   formatDate = (date) => {
+//     const options = { day: "numeric", month: "numeric", year: "numeric" };
+//     return new Intl.DateTimeFormat("en-GB", options).format(date);
+//   };
+
+//   //digunakan untuk menangani perubahan nilai pada input form. Nilai input akan diperbarui di dalam state sesuai dengan nama properti yang sesuai.
+//   handleChange = (event) => {
+//     const { name, value } = event.target;
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
+  
+
+//   render() {
+//     const { idBuku,nmBuku,penulis,thnTerbit,tglMasuk,jumlahBuku,noRak, showModal } = this.state;
+    
+//     return (
+//       <div className="container-2">
+//         <h6 style={{ color: "black", fontFamily: "sans-serif", fontWeight: "bold" }}>Form Peminjaman Buku</h6>
+//         <Form onSubmit={this.handleSubmit}>
+//           <Form.Group as={Row} controlId="formIdBuku">
+//             <Form.Label column sm={3} className="text-right">
+//               Id Buku
+//             </Form.Label>
+//             <Col sm={9}>
+//               <Form.Control
+//                 type="text"
+//                 name="idBuku"
+//                 value={idBuku}
+//                 onChange={this.handleChange}
+//                 required
+//               />
+//             </Col>
+//           </Form.Group>
+
+//           <Form.Group as={Row} controlId="formNamaBuku">
+//             <Form.Label column sm={3} className="text-right">
+//               Nama Buku
+//             </Form.Label>
+//             <Col sm={9}>
+//               <Form.Control
+//                 type="text"
+//                 name="nmBuku"
+//                 value={nmBuku}
+//                 onChange={this.handleChange}
+//                 required
+//               />
+//             </Col>
+//           </Form.Group>
+
+//           <Form.Group as={Row} controlId="formPenulis">
+//             <Form.Label column sm={3} className="text-right">
+//               Penulis Buku
+//             </Form.Label>
+//             <Col sm={9}>
+//               <Form.Control
+//                 type="text"
+//                 name="penulis"
+//                 value={penulis}
+//                 onChange={this.handleChange}
+//                 required
+//               />
+//             </Col>
+//           </Form.Group>
+
+//           <Form.Group as={Row} controlId="formThTerbit">
+//             <Form.Label column sm={3} className="text-right">
+//               Tahun Terbit
+//             </Form.Label>
+//             <Col sm={9}>
+//               <Form.Control
+//                 type="date"
+//                 name="thnTerbit"
+//                 value={thnTerbit}
+//                 onChange={this.handleChange}
+//                 required
+//               />
+//             </Col>
+//           </Form.Group>
+
+//           <Form.Group as={Row} controlId="formTglMasuk">
+//             <Form.Label column sm={3} className="text-right">
+//               Tanggal Masuk Buku
+//             </Form.Label>
+//             <Col sm={9}>
+//               <Form.Control
+//                 type="date"
+//                 name="tglMasuk"
+//                 value={tglMasuk}
+//                 onChange={this.handleChange}
+//                 required
+//               />
+//             </Col>
+//           </Form.Group>
+
+//           <Form.Group as={Row} controlId="formJumlahBuku">
+//             <Form.Label column sm={3} className="text-right">
+//               Jumlah Buku
+//             </Form.Label>
+//             <Col sm={9}>
+//               <Form.Control
+//                 type="text"
+//                 name="jumlahBuku"
+//                 value={jumlahBuku}
+//                 onChange={this.handleChange}
+//                 required
+//               />
+//             </Col>
+//           </Form.Group>
+
+//           <Form.Group as={Row} controlId="formNorak">
+//             <Form.Label column sm={3} className="text-right">
+//               No Rak Buku
+//             </Form.Label>
+//             <Col sm={9}>
+//               <Form.Control
+//                 type="text"
+//                 name="noRak"
+//                 value={noRak}
+//                 onChange={this.handleChange}
+//                 required
+//               />
+//             </Col>
+//           </Form.Group>
+
+//           <Form.Group as={Row}>
+//             <Col sm={{ span: 9, offset: 3 }}>
+//               <Button type="submit" variant="primary">
+//                 Simpan
+//               </Button>
+//             </Col>
+//           </Form.Group>
+//         </Form>
+
+//         {/* Modal */}
+//         <Modal show={showModal} onHide={this.handleModalClose}>
+//           <Modal.Header closeButton>
+//             <Modal.Title>Data Berhasil Ditambahkan</Modal.Title>
+//           </Modal.Header>
+//           <Modal.Body>
+//             <p>Data koleksi berhasil ditambahkan.</p>
+//           </Modal.Body>
+//           <Modal.Footer>
+//             <Button variant="primary" onClick={this.handleModalClose}>
+//               Oke
+//             </Button>
+//           </Modal.Footer>
+//         </Modal>
+
+//       </div>
+//     );
+//   }
+// }
+
+// export default FormCol;
+
 import React, { Component } from "react";
 import { Form, Button, Row, Col, Modal } from "react-bootstrap";
 import "./formC.css";
@@ -219,3 +443,4 @@ class FormCol extends Component {
 }
 
 export default FormCol;
+
--- src/modules/koleksi/tabel/TabelCol.js
+++ src/modules/koleksi/tabel/TabelCol.js
@@ -1,3 +1,183 @@
+// import React, { Component } from "react";
+// import { Table, Form, Row, Col, Button } from "react-bootstrap";
+// import { FaArrowAltCircleRight, FaMap, FaPlusCircle, FaPlus, FaEdit } from "react-icons/fa";
+// import Popup from "reactjs-popup";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./tabelcol.css";
+// import axios from "axios";
+// import FormCol from "../form/FormCol";
+
+
+// class TabelCol extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       searchQuery: "",
+//       isFormColOpen: false,
+//       koleksiData: [],
+//       selectedDat: null,
+//     };
+//     this.handleSearchChange = this.handleSearchChange.bind(this);
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   //digunakan untuk menangani perubahan nilai pada input pencarian
+//   handleSearchChange(event) {
+//     const searchQuery = event.target.value; //engakses nilai yang dimasukkan oleh pengguna 
+//     this.setState({
+//       searchQuery,//komponen akan melakukan re-rendering dan memperbarui tampilan dengan memfilter data berdasarkan nilai pencarian yang baru.
+
+
+//     });
+//   }
+
+//   //digunakan untuk menangani klik pada tombol "Tambahkan Data"
+//   handleClick() {
+//     this.setState((state) => ({
+//       isFormColOpen: true, //diubah menjadi true, maka komponen Popup yang mengandung FormCol akan ditampilkan, sehingga pengguna dapat memasukkan data baru ke dalam tabel koleksi buku.
+//     }));
+//   }
+
+
+//   //digunakan untuk menambahkan data baru ke dalam koleksi data dalam komponen TabelCol.
+//   addNewData = (newData) => {
+//     this.setState((prevState) => ({
+//       koleksiDataData: [...prevState.koleksiData, newData], //mengupdate properti koleksiData dalam state untuk mencakup item data baru (newData) dalam array koleksi data yang sudah ada (prevState.koleksiData).
+//       isFormColOpen: false,
+//     }));
+//     this.fetchData();
+//   };
+
+//   fetchData = ()=>{
+//     axios.get("http://localhost:4000/collection")
+//       .then((response) => {
+//         this.setState({ koleksiData: response.data }); //menangani respons dari server dengan mengupdate state koleksiData menggunakan data yang diterima dari respons (response.data).
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   }
+ 
+//   //menginisialisasi pengambilan data dari backend server pada saat komponen pertama kali dipasang.
+//   componentDidMount() {
+//     this.fetchData();
+//   }
+
+//   //digunakan untuk melakukan filter pada data koleksi dalam komponen TabelCol berdasarkan searchQuery yang ada dalam state.
+//   filterData = () => {
+//     const { koleksiData, searchQuery } = this.state;
+//     const filteredData = koleksiData.filter((data) => {
+//       const year = data.thnTerbit.toString(); // Konversi tahun terbit menjadi string
+//       return (
+//         data.idBuku.includes(searchQuery) ||
+//         data.nmBuku.toLowerCase().includes(searchQuery.toLowerCase()) ||
+//         data.penulis.toLowerCase().includes(searchQuery.toLowerCase()) ||
+//         year.includes(searchQuery) // Cari berdasarkan tahun terbit
+//       );
+//     });
+//     return filteredData;
+//   };
+
+//   handleEdit = (index) => {
+//     const { koleksiData } = this.state;
+//     const selectedData = koleksiData[index];
+//     this.setState({
+//       selectedData,
+//       isFormColOpen: true,
+//     });
+
+//     this.fetchData();
+//   };
+
+//   render() {
+//     const {isFormColOpen} = this.state;
+//     const filteredData = this.filterData();
+//     return (
+//       <React.Fragment>
+//         <div className="App">
+//           <h6 style={{ color: "white", fontFamily: "sans-serif", fontWeight: "bold" }}>
+//             Data Koleksi Buku
+//           </h6>
+//           <Row>
+//             <Col xs={7}>
+//               <Form.Control
+//                 type="text"
+//                 placeholder="Search Data"
+//                 onChange={this.handleSearchChange}
+//               />
+//             </Col>
+//             <Col xs={5} className="text-right">
+//               <Button
+//                 variant="light"
+//                 onClick={this.handleClick}
+//                 style={{
+//                   marginRight: "-100px",
+//                   height: "30px",
+//                   paddingTop: "2px",
+//                   borderRadius: "2px",
+//                   position: "relative",
+//                   marginTop: "15px",
+//                 }}
+//               >
+//                 <FaPlus className="mr-1" />
+//                 Tambahkan Data
+//               </Button>
+//             </Col>
+//           </Row>
+//           <br />
+
+
+//           <div className="table-responsive">
+//           <Table striped bordered hover responsive style={{ color: "#320B44", padding: "10px" }}>
+//             <thead>
+//               <tr>
+//                 <th>Id Buku</th>
+//                 <th>Nama Buku</th>
+//                 <th>Penulis</th>
+//                 <th>Tahun Terbit</th>
+//                 <th>Tanggal Masuk</th>
+//                 <th>Jumlah Buku</th>
+//                 <th>No Rak Buku</th>
+//                 <th>Aksi</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {filteredData.map((data, index) => (
+//                 <tr key={index}>
+//                   <td>{data.idBuku}</td>
+//                   <td>{data.nmBuku}</td>
+//                   <td>{data.penulis}</td>
+//                   <td>{data.thnTerbit}</td>
+//                   <td>{data.tglMasuk}</td>
+//                   <td>{data.jumlahBuku}</td>
+//                   <td>{data.noRak}</td>
+//                   <td>
+//                   <FaEdit className="icon-edit" onClick={() => this.handleEdit(index)} />
+//                   </td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </Table>
+//           </div>
+
+//           {isFormColOpen && (
+//             <Popup
+//               open
+//               closeOnDocumentClick
+//               onClose={() => this.setState({ isFormColOpen: false })}
+//             >
+//               <FormCol addNewData={this.addNewData} />
+//             </Popup>
+//           )}
+//         </div>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default TabelCol;
+
+
 import React, { Component } from "react";
 import { Table, Form, Row, Col, Button } from "react-bootstrap";
 import { FaArrowAltCircleRight, FaMap, FaPlusCircle, FaPlus, FaEdit } from "react-icons/fa";
@@ -37,11 +217,12 @@ class TabelCol extends Component {
 
   addNewData = (newData) => {
     this.setState((prevState) => ({
-      koleksiDataData: [...prevState.koleksiData, newData],
+      koleksiData: [...prevState.koleksiData, newData],
       isFormColOpen: false,
     }));
     this.fetchData();
   };
+  
 
   fetchData = ()=>{
     axios.get("http://localhost:4000/collection")
